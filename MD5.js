ea7702e37b46e2f3834e81fe955e28c111b91e7e
ETY001 <ety001@domyself.me>
2016-01-08 16:14:57 +0800

    remove script tags

--- MD5.js
+++ MD5.js
@@ -1,4 +1,3 @@
-<script type="text/javascript"> 
 /* 
 * A JavaScript implementation of the RSA Data Security, Inc. MD5 Message 
 * Digest Algorithm, as defined in RFC 1321. 
@@ -141,4 +140,3 @@ d = add(d, oldd);
 } 
 return rhex(a) + rhex(b) + rhex(c) + rhex(d); 
 }  
-</script>
